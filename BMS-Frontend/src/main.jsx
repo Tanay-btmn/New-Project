93592cca1de49b7c6f2b852a6b6f4431a9f0477e
Tanay <ttripathi1411@gmail.com>
2026-05-04 07:24:09 +0530

    debug: add mount check and logs to main.jsx

--- BMS-Frontend/src/main.jsx
+++ BMS-Frontend/src/main.jsx
@@ -3,8 +3,10 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
+console.log('Main.jsx is running');
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+    <div style={{color: 'white', padding: '20px'}}>React is mounting...</div>
     <App />
   </BrowserRouter>
 )
